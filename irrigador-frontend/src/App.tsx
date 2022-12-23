dbeadd622361069b7574441402ee9609e40d176d
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2022-12-23 09:26:24 -0300

    Adding Flowbite - A React component library

--- irrigador-frontend/src/App.tsx
+++ irrigador-frontend/src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <h1 className="text-2xl font-bold text-blue-500 underline">Hello World</h1>
+    <div className="min-h-screen bg-gradient-to-br from-green-400 to-blue-500">
+      <h1 className="text-2xl font-bold text-blue-500 underline">
+        Hello World
+      </h1>
+    </div>
   );
 }
 
