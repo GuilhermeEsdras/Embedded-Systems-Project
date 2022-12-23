03acf4754f226c3db8432072770a109b3dd9653c
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2022-12-23 00:31:05 -0300

    Frontend inital configurations

--- irrigador-frontend/vite.config.ts
+++ irrigador-frontend/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import eslint from "vite-plugin-eslint";
+
+const config = () => {
+  return defineConfig({
+    plugins: [react(), eslint(), splitVendorChunkPlugin()],
+    base: "./",
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+        "~components": path.resolve(__dirname, "./src/components"),
+        "~pages": path.resolve(__dirname, "./src/pages"),
+        "~styles": path.resolve(__dirname, "./src/styles"),
+      },
+    },
+    server: {
+      host: "localhost",
+      port: 5173,
+      base: "/",
+    },
+    esbuild: {
+      loader: "tsx",
+      logOverride: { "this-is-undefined-in-esm": "silent" },
+    },
+    build: {
+      outDir: "build",
+      watch: {
+        clearScreen: true,
+        buildDelay: 0,
+        include: "src/**",
+        exclude: "node_modules/**",
+      },
+    },
+  });
+};
+
+export default config;
