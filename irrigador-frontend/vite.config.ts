be2ad30d26503be4383da1118b3e6b35c329c0ea
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2022-12-23 11:56:02 -0300

    Refactoring path alias

--- irrigador-frontend/vite.config.ts
+++ irrigador-frontend/vite.config.ts
@@ -12,9 +12,6 @@ const config = () => {
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "./src"),
-        "~components": path.resolve(__dirname, "./src/components"),
-        "~pages": path.resolve(__dirname, "./src/pages"),
-        "~styles": path.resolve(__dirname, "./src/styles"),
       },
     },
     server: {
